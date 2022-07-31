af40c52af8718fbaf7b798602514e44cfbd1027a
Arturo González Villamizar <agvillamizar@gmail.com>
2022-07-31 11:56:11 -0500

    Reformat code.

--- kafka-streams-with-spring-cloud-stream/book-sales-stream/src/main/java/com/agvillamizar/booksalesstream/BookSalesStreamApplication.java
+++ kafka-streams-with-spring-cloud-stream/book-sales-stream/src/main/java/com/agvillamizar/booksalesstream/BookSalesStreamApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookSalesStreamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookSalesStreamApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookSalesStreamApplication.class, args);
+    }
 
 }
